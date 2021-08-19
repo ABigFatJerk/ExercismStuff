844df4b5de898941eee127c361325528e6133ce3
Aaron McConnell <mccoder@outlook.com>
2021-08-18 20:10:32 -0400

    Erlang Collatz Conjecture update

--- erlang/collatz-conjecture/src/collatz_conjecture.erl
+++ erlang/collatz-conjecture/src/collatz_conjecture.erl
@@ -7,7 +7,5 @@ steps(N) -> steps(N, 0).
 
 steps(N, _) when N =< 0 -> erlang:error(badarg);
 steps(1, Steps) -> Steps;
-steps(N, Steps) -> case N rem 2 == 0 of
-                     true -> steps(N div 2, Steps + 1);
-                     false -> steps((3*N)+1, Steps + 1)
-                   end.
+steps(N, Steps) when N rem 2 == 0 -> steps(N div 2, Steps + 1);
+steps(N, Steps) -> steps((3*N)+1, Steps + 1). 
