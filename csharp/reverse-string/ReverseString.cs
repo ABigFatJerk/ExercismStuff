cc95de48853d4e64bd58e6b236719e05ec4e73a6
Aaron McConnell <mccoder@outlook.com>
2021-08-20 22:24:08 -0400

    C# Reverse string and Sum of Multiples exercises

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        string result = "";
+        foreach (char c in input.Reverse<char>()) {
+            result += c;
+        }
+
+        return result;
+    }
+}
---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        IList<int> matches = new List<int>();
+        for (int i = 1; i < max; i++) {
+            if (IsMultiple(i, multiples)) {
+                matches.Add(i);
+            }
+        }
+        return matches.Sum();
+    }
+
+    private static bool IsMultiple(int number, IEnumerable<int> numbersToTest) {
+        return numbersToTest.Any(x => x > 0 && number % x == 0);
+    }
+}
