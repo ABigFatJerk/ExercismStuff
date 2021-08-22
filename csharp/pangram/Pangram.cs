6e0eab13e311871933dde7d365152c0b3809413f
Aaron McConnell <mccoder@outlook.com>
2021-08-21 22:53:01 -0400

    C# pangram exercise

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        // Pangram = The size of the set of lowercase letters in the input is 26
+        return new HashSet<char>(input.ToLower().Where(c => char.IsLetter(c))).Count == 26;
+    }
+}
