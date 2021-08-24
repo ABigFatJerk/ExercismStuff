96acae6b0fe1a85a845191bf3348502d3f31e9a5
Aaron McConnell <mccoder@outlook.com>
2021-08-23 21:28:29 -0400

    C# Armstrong number and Scrabble score exercises

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        IEnumerable<int> digits = Convert(number);
+        return number == digits.Select(x => (int)Math.Pow(x, digits.Count())).Sum();
+
+        // Alternate solution that doesn't need the Convert function but needs lots of type conversion
+        // string numberStr = number.ToString();
+        // return number == numberStr.Select(x => (int)Math.Pow(int.Parse(x.ToString()), numberStr.Length)).Sum();
+    }
+
+    private static IEnumerable<int> Convert(int number) {
+        List<int> result = new List<int>();
+
+        while (number > 0) {
+            result.Add(number % 10);
+            number /= 10;
+        }
+
+        return result;
+    }
+}
